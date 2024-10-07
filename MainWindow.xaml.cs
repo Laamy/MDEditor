cc3cc6341f42622b50d21d3e778c9247fbe48de2
yeemi routh <yeemirouth@gmail.com>
2024-10-08 10:28:56 +1100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+using Markdig;
+
+namespace MDEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // some sample markdown
+            {
+                var content = ResourceFetcher.GetResource("MDEditor.Content.ExampleMarkdown.md");
+                Paragraph sampleText = new Paragraph(new Run(content));
+
+                TextEditor.Document = new FlowDocument(sampleText);
+            }
+        }
+
+        private void TextEditor_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            string markdownText = new TextRange(TextEditor.Document.ContentStart, TextEditor.Document.ContentEnd).Text;
+
+            var pipeline = new MarkdownPipelineBuilder()
+                .UseAdvancedExtensions()
+                .UseEmojiAndSmiley()
+                .Build();
+
+            string htmlContent = Markdown.ToHtml(markdownText, pipeline);
+
+            var content = ResourceFetcher.GetResource("MDEditor.Content.MarkdownDisplay.html");
+            string styledContent = content.Replace("{HTMLContent}", htmlContent);
+
+            MarkdownDisplay.NavigateToString(styledContent);
+        }
+    }
+}
---
+++ SDK/ResourceFetcher.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+
+class ResourceFetcher
+{
+    public static string GetResource(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+        {
+            if (stream == null)
+            {
+                throw new FileNotFoundException("Embedded resource not found", resourceName);
+            }
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
